b19f6aaaa0046adde8abf8345b9d42187197a283
EnricoRuggieroB <45107998+EnricoRuggieroB@users.noreply.github.com>
2019-04-14 19:23:34 +0200

    Algoritmi di Laboratorio di Programmazione. Alunno: Bucchianico Enrico Ruggiero, 4^Finf.

--- thesum.py
+++ thesum.py
@@ -6,7 +6,7 @@
 
 Algoritmi "thesum": Algoritmo che dopo aver chiesto all'utente di inserire un numero intero da tastiera, lo somma ad un altro intero letto da un file di testo "sum.dat".
 Infine aggiorna il file "sum.dat" con il nuovo valore.
-Il programma iene conto del caso in cui il file "sum.dat" non esista.
+Il programma tiene conto del caso in cui il file "sum.dat" non esista.
 
 	- Algoritmo "thesum.py": Algoritmo "thesum" in Linguaggio Python.
 '''
